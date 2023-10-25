41e811b1fc3d74e6453b0acb4509ad30767ddf99
Sergii <sergii.web@gmail.com>
2023-10-25 15:40:22 +0300

    hw1_1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { UserController } from './user/user.controller';
+import { AuthController } from './auth/auth.controller';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, AuthModule, UserModule],
+  controllers: [AppController, UserController, AuthController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
